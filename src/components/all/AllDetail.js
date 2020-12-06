5e759170c6a485d26fdba610a3179177f2b13545
yeonsu <dustn1679@naver.com>
2020-12-06 15:12:08 +0900

    test3

--- src/components/all/AllDetail.js
+++ src/components/all/AllDetail.js
@@ -2,9 +2,22 @@ import React from 'react';
 
 const AllDetail = () => {
     return (
-        <div>
-            dd22
-        </div>
+        <>
+            {/* 뒤로가기 버튼 */}
+            <div>
+               &lt;
+            </div>
+
+            {/* 영상 */}
+            <div>
+                <iframe></iframe>
+            </div>
+
+            {/** 시청완료 버튼 */}
+            <div>
+                <button>시청완료</button>
+            </div>
+        </>
     );
 };
 
