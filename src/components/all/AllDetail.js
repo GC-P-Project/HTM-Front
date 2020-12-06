6e03c6a6441a28b9995c68b5b90b47066cd9ca4f
yeonsu <dustn1679@naver.com>
2020-12-07 01:32:21 +0900

    Yeonsu All 수정
    AllDetail.js AllWrap.js 수정, All.css 생성

--- src/components/all/AllDetail.js
+++ src/components/all/AllDetail.js
@@ -1,23 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./AllWrap.css";
 
 const AllDetail = () => {
     return (
-        <>
+        <div className="container">
             {/* 뒤로가기 버튼 */}
-            <div>
-               &lt;
+            <div className="mb-4">
+                <Link to="#">
+                    <svg className="backIcon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 256 256" id="Layer_1" version="1.1" viewBox="0 0 256 256">
+                        <path d="M179.199,38.399c0,1.637-0.625,3.274-1.875,4.524l-85.076,85.075l85.076,85.075c2.5,2.5,2.5,6.55,0,9.05s-6.55,2.5-9.05,0  l-89.601-89.6c-2.5-2.5-2.5-6.551,0-9.051l89.601-89.6c2.5-2.5,6.55-2.5,9.05,0C178.574,35.124,179.199,36.762,179.199,38.399z" />
+                    </svg>
+                </Link>
             </div>
 
             {/* 영상 */}
-            <div>
-                <iframe></iframe>
+            <div className="embed-responsive embed-responsive-16by9 mb-4">
+                <iframe
+                    src="https://www.youtube.com/embed/dBtk6T-aWQ4"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowfullscreen="true">
+                </iframe>
             </div>
 
             {/** 시청완료 버튼 */}
-            <div>
-                <button>시청완료</button>
+            <div className="text-center">
+                <button className="btn btnDone">시청완료</button>
             </div>
-        </>
+        </div>
     );
 };
 
--- src/components/all/AllWrap.js
+++ src/components/all/AllWrap.js
@@ -1,9 +1,61 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import "./All.css";
+
 
 const AllWrap = () => {
     return (
-        <div>
-            전체
+        <div className="container">
+
+            {/** upper menu 1 */}
+            <div className="row">
+                <div className="col-3">
+                    <button className="btn btnUpper">등</button>
+                </div>
+                <div className="col-3">
+                    어깨
+                </div>
+                <div className="col-3">
+                    가슴
+                </div>
+                <div className="col-3">
+                    허리
+                </div>
+            </div>
+
+            {/** upper menu 2 */}
+            <div>
+                <ul className="nav nav-pills">
+                    <li className="nav-item">
+                        <a className="nav-link active">Active</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" >Link</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" >Disabled</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+
+            {/** listView */}
+            {/** for */}
+            <div className="row listView">
+                <div className="col-6">
+                    <Link to="/all_detail/:idx">
+                        <img src="https://via.placeholder.com/1600x900" className="img-fluid"></img>
+                    </Link>
+                </div>
+                <div className="col-6">
+                    제목<br />운동부위<br />설명<br />시간<br />??
+                    </div>
+            </div>
+            {/** endfor */}
+
+
         </div>
     );
 };
