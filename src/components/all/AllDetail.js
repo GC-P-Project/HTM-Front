2cbcd7e873d6f1bc3c0db7bb15e6c2df799f7aa2
yeonsu <dustn1679@naver.com>
2020-12-08 04:34:56 +0900

    Yeonsu All 수정
    리스트뷰, 뒤로가기 구현

--- src/components/all/AllDetail.js
+++ src/components/all/AllDetail.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./AllWrap.css";
+import "./All.css";
 
 const AllDetail = () => {
+
+    {/* 뒤로가기 */ }
+    const goBack = () => {
+        window.history.back();
+    };
+
     return (
         <div className="container">
+
             {/* 뒤로가기 버튼 */}
             <div className="mb-4">
-                <Link to="#">
+                <a onClick={goBack}>
                     <svg className="backIcon" xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 256 256" id="Layer_1" version="1.1" viewBox="0 0 256 256">
                         <path d="M179.199,38.399c0,1.637-0.625,3.274-1.875,4.524l-85.076,85.075l85.076,85.075c2.5,2.5,2.5,6.55,0,9.05s-6.55,2.5-9.05,0  l-89.601-89.6c-2.5-2.5-2.5-6.551,0-9.051l89.601-89.6c2.5-2.5,6.55-2.5,9.05,0C178.574,35.124,179.199,36.762,179.199,38.399z" />
                     </svg>
-                </Link>
+                </a>
             </div>
 
             {/* 영상 */}
--- src/components/all/AllWrap.js
+++ src/components/all/AllWrap.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import "./All.css";
 
-
 const AllWrap = () => {
     return (
         <div className="container">
@@ -40,21 +39,46 @@ const AllWrap = () => {
                     </li>
                 </ul>
             </div>
+            <hr></hr>
 
-            {/** listView */}
-            {/** for */}
+
+
+
+            {/** card listView */}
             <div className="row listView">
-                <div className="col-6">
+                {/** thumbnail */}
+                <div className="col-6 thumb">
                     <Link to="/all_detail/:idx">
                         <img src="https://via.placeholder.com/1600x900" className="img-fluid"></img>
                     </Link>
                 </div>
-                <div className="col-6">
-                    제목<br />운동부위<br />설명<br />시간<br />??
-                    </div>
+                {/** descriptions */}
+                <div className="col-6 desc">
+                    <p style={{ fontSize: "14px" }}>영상제목</p>
+                    <hr className="my-2" />
+                    <p>
+                        영상시간, 운동부위 관련 설명
+                    </p>
+                </div>
             </div>
-            {/** endfor */}
 
+            {/** example */}
+            <div className="row listView">
+                {/** thumbnail */}
+                <div className="col-6">
+                    <Link to="/all_detail/:idx">
+                        <img src="https://i.ytimg.com/an_webp/dBtk6T-aWQ4/mqdefault_6s.webp?du=3000&sqp=CMKmuf4F&rs=AOn4CLDC2MVf_PrkCk5BMI4dEzEOw5cnkw" className="img-fluid"></img>
+                    </Link>
+                </div>
+                {/** descriptions */}
+                <div className="col-6">
+                    <p className="title">하체비만 탈출 6분 프로그램 Legs fat burn workout for women하체비만 탈출 6분 프로그램 Legs fat burn workout for women하체비만 탈출 6분 프로그램 Legs fat burn workout for women</p>
+                    <hr className="my-2" />
+                    <p className="desc">
+                        10:02 하체운동
+                    </p>
+                </div>
+            </div>
 
         </div>
     );
