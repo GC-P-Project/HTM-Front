35142a640b75360005d64181e02430918eab53ca
yeonsu <dustn1679@naver.com>
2020-12-14 20:34:54 +0900

    .

--- src/components/video/Detail.js
+++ src/components/video/Detail.js
@@ -6,6 +6,42 @@ const Detail = (props) => {
     const urlData = props.match.params.idx
     const [data, setData] = useState([""]);
 
+    const loginActive = window.sessionStorage.getItem("token");
+    const loginFlag = (loginActive !== null) ? true : false;
+
+    console.log(loginActive)
+
+
+    // Star Rating Data
+    const [ratingData, setRatingData] = useState({ num: 3, });
+    const { num } = ratingData;
+    const starRatingData = (e) => {
+        const index = e.target.value;
+        setRatingData({
+            ...ratingData,
+            num: index,
+        });
+    };
+
+
+    // Video Complete
+    const videoComplete = async (ratingData) => {
+        const response = await fetch("http://54.180.123.156:8080/user/signIn333", {
+            method: "POST",
+            headers: { "Content-Type": "application/json", },
+            body: JSON.stringify({
+                // token header? loginActive?
+            }),
+        })
+            .then(() => {
+                // disable button?
+            })
+            .catch((e) => {
+                console.log(e);
+            });
+        return response;
+    };
+
 
     // call API
     useEffect(() => {
@@ -41,12 +77,12 @@ const Detail = (props) => {
     return (
         <div className="container">
             {/* 뒤로가기 버튼 */}
-            <div className="mb-5">
-                <a onClick={goBack}>
+            <div className="mb-4">
+                <p onClick={goBack}>
                     <svg className="backIcon" xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 256 256" id="Layer_1" version="1.1" viewBox="0 0 256 256">
                         <path d="M179.199,38.399c0,1.637-0.625,3.274-1.875,4.524l-85.076,85.075l85.076,85.075c2.5,2.5,2.5,6.55,0,9.05s-6.55,2.5-9.05,0  l-89.601-89.6c-2.5-2.5-2.5-6.551,0-9.051l89.601-89.6c2.5-2.5,6.55-2.5,9.05,0C178.574,35.124,179.199,36.762,179.199,38.399z" />
                     </svg>
-                </a>
+                </p>
             </div>
 
 
@@ -65,18 +101,18 @@ const Detail = (props) => {
             <div className="starBox mb-3">
                 <p>별점을 기반으로 영상을 추천해 드립니다</p>
                 <div className="rating">
-                    <input type="radio" name="rating" value="5" id="5" /><label htmlFor="5">☆</label>
-                    <input type="radio" name="rating" value="4" id="4" /><label htmlFor="4">☆</label>
-                    <input type="radio" name="rating" value="3" id="3" /><label htmlFor="3">☆</label>
-                    <input type="radio" name="rating" value="2" id="2" /><label htmlFor="2">☆</label>
-                    <input type="radio" name="rating" value="1" id="1" /><label htmlFor="1">☆</label>
+                    <input type="radio" name="rating" value="5" id="5" onClick={starRatingData} /><label htmlFor="5">☆</label>
+                    <input type="radio" name="rating" value="4" id="4" onClick={starRatingData} /><label htmlFor="4">☆</label>
+                    <input type="radio" name="rating" value="3" id="3" onClick={starRatingData} /><label htmlFor="3">☆</label>
+                    <input type="radio" name="rating" value="2" id="2" onClick={starRatingData} /><label htmlFor="2">☆</label>
+                    <input type="radio" name="rating" value="1" id="1" onClick={starRatingData} /><label htmlFor="1">☆</label>
                 </div>
             </div>
 
 
             {/** 시청완료 버튼 */}
             <div className="text-center">
-                <button className="btn btnDone">시청완료</button>
+                <button className="btn btnDone" onClick={videoComplete}>시청완료</button>
             </div>
         </div>
     );
